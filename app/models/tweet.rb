1b5fd2f892b9b867c4d4c90f2c78b3806dab989a
Amy Flynn <aeflynn20@gmail.com>
2017-10-23 18:50:47 -0500

    Add styles and validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
+validates :message, presence: true, length: { maximum: 140, too_long: "Count your characters! Tweets are only 140 characters long."}
 
 end
